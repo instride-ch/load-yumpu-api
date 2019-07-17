c8b3129dc35388f03ac48bfef756b9591c846a22
Aaron Gerig <a.gerig@w-vision.ch>
2019-07-17 10:10:48 +0200

    refactor: make it a real ES6 module and use modern syntax, resolves #1

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var promise = null
+let promise = null
 
-module.exports = function () {
-  if (!promise) {
-    promise = new Promise(function (resolve, reject) {
+export default function loadYumpuApi () {
+  promise =
+    promise ||
+    new Promise((resolve, reject) => {
       // Prepare the `script` tag to be inserted into the page
-      var scriptElement = document.createElement('script')
+      const scriptElement = document.createElement('script')
       scriptElement.src = 'https://players.yumpu.com/hub.js'
 
       // Resolve promise once script has loaded
-      scriptElement.onload = function () {
+      scriptElement.onload = () => {
         resolve(window.Yumpu)
       }
 
       // Reject promise if an error occurs
-      scriptElement.onerror = function () {
+      scriptElement.onerror = () => {
         reject(new Error('Could not load the Yumpu API'))
       }
 
       // Insert the `script` tag
       document.body.appendChild(scriptElement)
     })
-  }
 
   return promise
 }
