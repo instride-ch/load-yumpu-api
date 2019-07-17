75074e125b680a55bd7706cd4bdcc6903c18d439
Aaron Gerig <a.gerig@w-vision.ch>
2019-07-17 10:14:55 +0200

    test: added missing test

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-const test = require('tape')
-const loadYumpuAPI = require('./')
+import test from 'tape'
+import loadYumpuApi from './'
 
-test('resolves the promise to the `Yumpu` object with support for duplicate calls', function (t) {
+test('resolves the promise to the `Yumpu` object', function (t) {
+  t.plan(1)
+
+  loadYumpuApi().then(function (Yumpu) {
+    t.true(typeof Yumpu === 'function')
+  }, t.fail)
+})
+
+test('resolves the promise to the `Yumpu` object, with support for duplicate calls', function (t) {
   t.plan(2)
 
-  const promises = [loadYumpuAPI(), loadYumpuAPI()]
+  const promises = [loadYumpuApi(), loadYumpuApi()]
 
   Promise.all(promises).then(function (values) {
     t.equal(values[0], values[1])
